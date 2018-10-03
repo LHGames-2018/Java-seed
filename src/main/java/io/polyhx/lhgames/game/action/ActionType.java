6e0b32bd9c50e7e775eee0fa8540eab4520c98ef
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-03 13:46:26 -0400

    Add IAction interface

---
+++ src/main/java/io/polyhx/lhgames/game/action/ActionType.java
@@ -0,0 +1,21 @@
+package io.polyhx.lhgames.game.action;
+
+public enum ActionType {
+    STEAL("StealAction"),
+    MELEE_ATTACK("MeleeAttackAction"),
+    COLLECT("CollectAction"),
+    MOVE("MoveAction"),
+    UPGRADE("UpgradeAction"),
+    PURCHASE("PurchaseAction"),
+    HEAL("HealAction");
+
+    private final String fJSON;
+
+    ActionType(String json) {
+        fJSON = json;
+    }
+
+    public String getJSON() {
+        return fJSON;
+    }
+}
---
+++ src/main/java/io/polyhx/lhgames/game/action/IAction.java
@@ -0,0 +1,6 @@
+package io.polyhx.lhgames.game.action;
+
+public interface IAction {
+    ActionType getActionType();
+    String getJSONContent();
+}
