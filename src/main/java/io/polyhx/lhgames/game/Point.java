ea028d27012af3cf64cb7c5fffc6586d99c0cefc
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-03 13:44:01 -0400

    Add toJSON() and common direction to the Point class

--- src/main/java/io/polyhx/lhgames/game/Point.java
+++ src/main/java/io/polyhx/lhgames/game/Point.java
@@ -14,11 +14,23 @@ public class Point {
         fY = y;
     }
 
+    public static final Point UP = new Point(0, -1);
+
+    public static final Point DOWN = new Point(0, 1);
+
+    public static final Point LEFT = new Point(-1, 0);
+
+    public static final Point RIGHT = new Point(1, 0);
+
     public int getX() {
         return fX;
     }
 
     public int getY() {
         return fY;
+    }
+
+    public String toJSON() {
+        return String.format("{X:%d,Y:%d}", fX, fY);
     }
 }
