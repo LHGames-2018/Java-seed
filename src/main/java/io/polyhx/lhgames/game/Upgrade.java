cab15eecc65c8b247368be886244985427cd3d28
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-03 13:56:50 -0400

    Add ID to Upgrade enumeration

--- src/main/java/io/polyhx/lhgames/game/Upgrade.java
+++ src/main/java/io/polyhx/lhgames/game/Upgrade.java
@@ -1,19 +1,25 @@
 package io.polyhx.lhgames.game;
 
 public enum Upgrade {
-    CARRYING_CAPACITY("CarryingCapacity"),
-    MAXIMUM_HEALTH("MaximumHealth"),
-    COLLECTING_SPEED("CollectingSpeed"),
-    ATTACK("AttackPower"),
-    DEFENCE("Defence");
+    CARRYING_CAPACITY("CarryingCapacity", 0),
+    MAXIMUM_HEALTH("MaximumHealth", 3),
+    COLLECTING_SPEED("CollectingSpeed", 4),
+    ATTACK("AttackPower", 1),
+    DEFENCE("Defence", 2);
 
     private final String fJSON;
+    private final int fID;
 
-    Upgrade(String json) {
+    Upgrade(String json, int id) {
         fJSON = json;
+        fID = id;
     }
 
     public String getJSON() {
         return fJSON;
+    }
+
+    public int getID() {
+        return fID;
     }
 }
