ed9e801f623154097fc9931c72546f6591456593
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-04 19:15:11 -0400

    Transform the point to a direction in AbstractPointAction

--- src/main/java/io/polyhx/lhgames/game/action/AbstractPointAction.java
+++ src/main/java/io/polyhx/lhgames/game/action/AbstractPointAction.java
@@ -10,7 +10,12 @@ public abstract class AbstractPointAction implements IAction {
     private final Point fPoint;
 
     public AbstractPointAction(Point point) {
-        fPoint = point;
+        /* get the direction for each coordinate */
+        int x = point.getX()/Math.abs(point.getX());
+        int y = point.getY()/Math.abs(point.getY());
+
+        /* the point can only be in one direction */
+        fPoint = (Math.abs(x) + Math.abs(y) == 2) ? new Point() : new Point(x, y);
     }
 
     @Override
