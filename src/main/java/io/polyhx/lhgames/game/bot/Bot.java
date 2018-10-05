7c46005d6f7994bbb3f93292dff5a8cdbad409a4
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-05 18:46:27 -0400

    Remove unused import

--- src/main/java/io/polyhx/lhgames/game/bot/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/bot/Bot.java
@@ -3,7 +3,6 @@ package io.polyhx.lhgames.game.bot;
 import io.polyhx.lhgames.game.Map;
 import io.polyhx.lhgames.game.Player;
 import io.polyhx.lhgames.game.action.IAction;
-import io.polyhx.lhgames.game.action.MoveAction;
 import io.polyhx.lhgames.game.point.Point;
 
 public class Bot extends BaseBot {
