154d1f5e36bde10863dcaa2aae620378892cbff6
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2018-10-05 18:32:25 -0400

    Add missing import

--- src/main/java/io/polyhx/lhgames/game/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/Bot.java
@@ -2,6 +2,7 @@ package io.polyhx.lhgames.game;
 
 import io.polyhx.lhgames.game.action.IAction;
 import io.polyhx.lhgames.game.action.MoveAction;
+import io.polyhx.lhgames.game.point.Point;
 
 public class Bot {
     public IAction getAction(Player player, Map map) {
